044629ff5859f0f76e359638e25fbb60027d7d66
Marshal Welgama <marshalwelgama@gmail.com>
2022-03-27 22:30:56 +1100

    new: spinner working and layout for textedit corrected

--- UnitConverter/app/src/main/java/com/example/unit_converter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.unit_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner measurementSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        measurementSpinner = (Spinner) findViewById(R.id.selection);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.listMeasurements));
+        measurementSpinner.setAdapter(adapter);
     }
 }
