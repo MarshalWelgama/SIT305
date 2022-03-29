877aedf5da695c61e845de124b46493abeb9a594
Marshal Welgama <marshalwelgama@gmail.com>
2022-03-29 23:51:34 +1100

    new: added helper functions, and functionality for tempreature and weight

--- UnitConverter/app/src/main/java/com/example/unit_converter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/unit_converter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.unit_converter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,7 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // initializing variable types
+    // INITIALIZING VARIABLES
     Spinner measurementSpinner;
     ImageButton measurementButton, temperatureButton, weightButton;
     TextView result1, result2, result3, unit1, unit2, unit3;
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // linking variable to layout
+
+        // LINKING VARIABLES TO LAYOUT
         inputUnit = (EditText) findViewById(R.id.inputUnit);
         measurementSpinner = (Spinner) findViewById(R.id.selection);
         measurementButton = (ImageButton) findViewById(R.id.measurementButton);
@@ -36,18 +38,19 @@ public class MainActivity extends AppCompatActivity {
         unit2 = (TextView) findViewById(R.id.unit2);
         unit3 = (TextView) findViewById(R.id.unit3);
 
-        //setting selections for dropdown menu
+        //SETTING SELECTIONS
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item,
                 getResources().getStringArray(R.array.listMeasurements));
         measurementSpinner.setAdapter(adapter);
 
+        //BUTTON CLICK FUNCTIONS
         measurementButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 if (measurementSpinner.getSelectedItemPosition() != 0) {
                     Toast.makeText(getApplicationContext(), "Please select the correct conversion icon", Toast.LENGTH_SHORT).show();
-
+                    return;
                 }
                 try {
                     measurementHandler(Float.parseFloat(inputUnit.getText().toString()));
@@ -58,28 +61,85 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             });
-        //do onClick for the other buttons
+
+        temperatureButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (measurementSpinner.getSelectedItemPosition() != 1) {
+                    Toast.makeText(getApplicationContext(), "Please select the correct conversion icon", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    temperatureHandler(Float.parseFloat(inputUnit.getText().toString()));
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Please enter a number! Error: "+ e.getClass(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        weightButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (measurementSpinner.getSelectedItemPosition() != 2) {
+                    Toast.makeText(getApplicationContext(), "Please select the correct conversion icon", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try {
+                    weightHandler(Float.parseFloat(inputUnit.getText().toString()));
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Please enter a number! Error Code: "+ e.getClass(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         };
 
-
+    // CALCULATION FUNCTIONS
     public void measurementHandler (Float input) {
         String centimetre = String.format("%.2f", input*100);
         String foot = String.format("%.2f", input*3.28);
         String inches = String.format("%.2f", input*39.37);
-        result1.setText(centimetre);
-        result2.setText(foot);
-        result3.setText(inches);
-        unit1.setText("Centemetres");
-        unit2.setText("Foot");
-        unit3.setText("Inches");
-
+        setResult(centimetre,foot,inches);
+        setUnit("Centemetres", "Foot", "Inches");
     }
     public void temperatureHandler (float input) { //do temps
-
+        String fahrenheit = String.format("%.2f", (input *1.8) + 32); //find temps needed
+        String kelvin = String.format("%.2f", input+273.15);
+        setResult(fahrenheit, kelvin, "");
+        setUnit("Fahrenheit", "Kelvin", "");
     }
 
     public void weightHandler (float input) { //do weights
+        String gram = String.format("%.2f", input*1000); //find weight needed
+        String ounce = String.format("%.2f", input*35.27);
+        String pound = String.format("%.2f", input*2.2);
+        setResult(gram,ounce,pound);
+        setUnit("Gram", "Ounce(Oz)", "Pound(lb)");
 
+
+    }
+
+    //HELPER FUNCTIONS
+    public void setResult (String val1, String val2, @Nullable String val3){
+        if(val1.length() > 1) {
+            result1.setText(val1);
+        }
+        if(val2.length() > 1) {
+            result2.setText(val2);
+        }
+        if(val3.length() > 1) {
+            result3.setText(val3);
+        }
+    }
+    public void setUnit (String val1, String val2, @Nullable String val3){
+        if(val1.length() > 0) {
+            unit1.setText(val1);
+        }
+        if(val2.length() > 0) {
+            unit2.setText(val2);
+        }
+        if(val3.length() > 0) {
+            unit3.setText(val3);
+        }
     }
 }
 
