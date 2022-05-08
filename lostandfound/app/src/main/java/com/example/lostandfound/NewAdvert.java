35187ce06a9f46f2ef27db3c11a49e004bcb3c39
Marshal Welgama <marshalwelgama@gmail.com>
2022-05-08 20:39:08 +1000

    new: added layout for new pages, linked onclick on main

--- lostandfound/app/src/main/java/com/example/lostandfound/NewAdvert.java
+++ lostandfound/app/src/main/java/com/example/lostandfound/NewAdvert.java
@@ -1,7 +1,17 @@
 package com.example.lostandfound;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewAdvert extends AppCompatActivity {
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newadvert);
+
+//        txtNoteContent = findViewById(R.id.txtNoteContent);
+//        btnSaveEdit = findViewById(R.id.btnDelete);
+//        db = new DatabaseHelper(this);
+    }
 }
