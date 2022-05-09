5cec1a0d84efb17991342765b538299af45f96bc
Marshal Welgama <marshalwelgama@gmail.com>
2022-05-09 23:08:45 +1000

    new: added db, model and util class

--- lostandfound/app/src/main/java/com/example/lostandfound/NewAdvert.java
+++ lostandfound/app/src/main/java/com/example/lostandfound/NewAdvert.java
@@ -1,17 +1,46 @@
 package com.example.lostandfound;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.lostandfound.data.DatabaseHelper;
+
 public class NewAdvert extends AppCompatActivity {
+    EditText NameField;
+    EditText PhoneField;
+    EditText DescriptionField;
+    EditText DateField;
+    EditText LocationField;
+    Button SaveBtn;
+    DatabaseHelper db;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newadvert);
+        db = new DatabaseHelper(this);
+        NameField = (EditText) findViewById(R.id.NameField);
+        PhoneField = (EditText) findViewById(R.id.PhoneField);
+        DescriptionField = (EditText) findViewById(R.id.DescriptionField);
+        DateField = (EditText) findViewById(R.id.DateField);
+        LocationField = (EditText) findViewById(R.id.LocationField);
+        SaveBtn = (Button) findViewById(R.id.SaveBtn);
 
-//        txtNoteContent = findViewById(R.id.txtNoteContent);
-//        btnSaveEdit = findViewById(R.id.btnDelete);
-//        db = new DatabaseHelper(this);
+        SaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = NameField.getText().toString();
+                String phone = PhoneField.getText().toString();
+                String Description = DescriptionField.getText().toString();
+                String Date = DateField.getText().toString();
+                String Location = LocationField.getText().toString();
+
+            }
+        });
+
+
     }
 }
